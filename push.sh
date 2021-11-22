66cc04c4cd254c5f54011710b9c7ff2b8ca2c6dc
Renan <desen.renan@gmail.com>
2021-11-22 08:22:04 -0300

    Testando push com sh

--- push.sh
+++ push.sh
@@ -1,6 +1,37 @@
 #!/bin/bash
-git status
+
+# get current branch and push
+current_branch=$(git branch | sed -n -e 's/^\* \(.*\)/\1/p')
+
+# git pull
+#git pull origin "$current_branch"
+#echo "==== BAIXANDO DA '$current_branch' BRANCH"
+
+# get the argument message
+message="$1"
+
+Username="j-renan"
+
+Password="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"
+
+
+# If no commit message is passed, use current date time in the commit message
+if [[ -z "${message// }" ]]
+    then
+        message=$(date '+%Y-%m-%d %H:%M:%S')
+fi
+
+# stage all changes
 git add --all
-git commit -m ""
-git push origin master
+echo "==== ADD TODOS OS ARQUIVOS"
+
+# add commit
+git commit -m "$message"
+echo "==== ADICIONE UM COMMIT: '$message'"
+
+# git push
+git push origin "$current_branch"
+echo "==== PUSH ALTERAÇÕES '$current_branch' BRANCH"
+
+
 
